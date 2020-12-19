947af4624df8c0d5ca96e1e2f5ccfc00f6e41b12
DJFriar <tommy.craft@icloud.com>
2020-12-18 22:48:57 -0600

    Setup rough scaffold

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # SMU-Homework-11-Note-Taker
 
-SMU Homework 11 - Note Taker
+PUT PROJECT DESCRIPTION HERE
+
+## Installation
+
+INSERT INSTALL INSTRUCTIONS HERE
+
+## Usage
+
+INSERT SCREENSHOTS HERE
+
+## Credits
+
+PUT CREDITS HERE
+
+## License
+
+![license](https://img.shields.io/badge/license-MIT-brightgreen)
---
+++ routes.js
@@ -0,0 +1,43 @@
+var path = require("path");
+var noteData = require("./noteData");
+
+// ===============================================================================
+// HTML ROUTES
+// ===============================================================================
+
+module.exports = function(app) {
+  app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
+  });
+
+  // If no matching route is found default to home
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+};
+
+// ===============================================================================
+// API ROUTES
+// ===============================================================================
+
+module.exports = function(app) {
+
+  // API GET Requests
+  app.get("/api/notes", function(req, res) {
+    res.json(noteData);
+  });
+
+  // API POST Requests
+  app.post("/api/notes", function(req, res) {
+    if (noteData.length < 5) {
+      noteData.push(req.body);
+      res.json(true);
+    }
+  });
+
+  // Reset All Data **REMOVE BEFORE FINAL**
+  app.post("/api/reset", function(req, res) {
+    noteData.length = 0;
+    res.json({ ok: true });
+  });
+};
